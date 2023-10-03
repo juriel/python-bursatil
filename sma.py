f3878553bfbd9a3e574bf5b82d16a2c64d3d2f26
juriel <juriel@comtor.net>
2023-10-02 20:47:37 -0500

    cambios en SMA

--- sma.py
+++ sma.py
@@ -4,20 +4,24 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
 
-desired_win = 1.06
-stop_loss   = 0.9
+# Definir el símbolo de la acción
+TICKER_SYMBOL = "NVDA" 
 
 
-desired_loss = 0.94
-stop_short   = 1.1
+DESIRED_WIN = 1.04
+STOP_LOSS   = 0.9
+
+
+DESIRED_LOSS = 0.96
+STOP_SHORT   = 1.1
 
 
 def simulate_buy(price,data):
     for index, row in data.iterrows():
-        if row["Low"]  < price*stop_loss :
+        if row["Low"]  < price*STOP_LOSS :
             return row["Low"]/price
-        if row["High"] > price* desired_win:
-            return   desired_win
+        if row["High"] > price* DESIRED_WIN:
+            return   DESIRED_WIN
 
     last_row = data.iloc[-1]
     return last_row["Close"]/price
@@ -25,25 +29,23 @@ def simulate_buy(price,data):
 
 def simulate_sell(price,data):
     for index, row in data.iterrows():
-        if row["High"]  > price*stop_short :
+        if row["High"]  > price*STOP_SHORT :
             return price/row["High"]
-        if row["Low"] <  price* desired_loss:
-            return   1.0/desired_loss
+        if row["Low"] <  price* DESIRED_LOSS:
+            return   1.0/DESIRED_LOSS
     try:
         last_row = data.iloc[-1]
         return last_row["Close"]/price
     except:
         return -1
 
-# Definir el símbolo de la acción
-ticker_symbol = "NVDA" 
 
 # Calcular las fechas hace dos años y la fecha actual
 end_date   = datetime.today().strftime('%Y-%m-%d')
 start_date = (datetime.today() - timedelta(days=3650)).strftime('%Y-%m-%d')  
 
 #En la variable va a quedar el histórico
-data = yf.download(ticker_symbol, start=start_date)
+data = yf.download(TICKER_SYMBOL, start=start_date)
 
 long_sma = 40
 short_sma = 10
@@ -62,7 +64,7 @@ data[delta_sma_short_colname] = ((data[sma_short_colname] - data[sma_short_colna
 data = data.dropna()
 
 
-
+data = data.copy()
 data['Recomendacion'] = 'Esperar'  # Valor predeterminado de "Esperar"
 data.loc[ (data[delta_sma_long_colname] > 0) & (data[delta_sma_short_colname] > 0), 'Recomendacion' ] = 'Compra'
 data.loc[ (data[delta_sma_long_colname] < 0) & (data[delta_sma_short_colname] < 0), 'Recomendacion' ] = 'Venta'
@@ -70,36 +72,42 @@ data.loc[ (data[delta_sma_long_colname] < 0) & (data[delta_sma_short_colname] < 
 
 
 
-
-
 future_window = 20
 future_high =  'high_'+str(future_window) +'next'
 future_low  =  'low_'+str(future_window) +'next'
-data[future_high] =  data['High'].rolling(future_window).max().shift(-1*future_window)
-data[future_low]  =  data['Low'].rolling(future_window).min().shift(-1*future_window)
+col = data['High'].rolling(future_window).max().shift(-1*future_window)
+data[future_high] =  col
+col = data['Low'].rolling(future_window).min().shift(-1*future_window)
+data[future_low]  =  col
 
 
 
 
 data['Backtest'] = 'Esperar'      # Valor predeterminado de "Esperar"
-data.loc[ (data[future_high] >= data["Close"]*desired_win) & (data[future_low]  >  data["Close"]*stop_loss), 'Backtest' ] = 'Compra'
-data.loc[ (data[future_low]  < data["Close"]*desired_loss)  & (data[future_high] < data["Close"]*stop_short) ,'Backtest' ] = 'Venta'
+data.loc[ (data[future_high] >= data["Close"]*DESIRED_WIN) & (data[future_low]  >  data["Close"]*STOP_LOSS), 'Backtest' ] = 'Compra'
+data.loc[ (data[future_low]  < data["Close"]*DESIRED_LOSS)  & (data[future_high] < data["Close"]*STOP_SHORT) ,'Backtest' ] = 'Venta'
 
-
-
+data = data.copy()
 data["simulate"] = 0
 i = 0
 num_rows = data.shape[0]
+
+
+
 for index, row in data.iterrows():
-    if (row["Recomendacion"] == "Compra"):
-        data.at[index,"simulate"] =simulate_buy(row["Close"], data.tail(num_rows-i-1).head(100))
-    if (row["Recomendacion"] == "Venta"):
-        data.at[index,"simulate"] =simulate_sell(row["Close"], data.tail(num_rows-i-1).head(100))        
+    #print("HELLO------------------------------",index, row)
+    simulation_rows = data.tail(num_rows-i-1).head(100)
+    if (row["Recomendacion"] == "Compra") and simulation_rows.shape[0] > 0:
+        data.at[index,"simulate"] = simulate_buy(row["Close"], simulation_rows)
+    if (row["Recomendacion"] == "Venta") and simulation_rows.shape[0] > 0:
+        data.at[index,"simulate"] = simulate_sell(row["Close"], simulation_rows)        
     i  = i+1
 
 
-excel_filename = "DATA/"+ticker_symbol + "_historico_precios3.xlsx"
+excel_filename = "DATA/"+TICKER_SYMBOL + "_historico_precios3.xlsx"
 data.to_excel(excel_filename)
+print(data)
+
 
 print(data)
 
@@ -124,13 +132,10 @@ for index, row in data.iterrows():
 
 for index, row in data.iterrows():
     color = "gray"
-    print("simulate",row['simulate'], type(row['simulate']))
     if row['simulate'] > 1 :
-        print("GREEN")
         color = "green"
 
-    if 0 < row['simulate'] and row['simulate'] < 1:
-        print("ROJO")
+    if 0 < row['simulate'] and row['simulate'] < 1:        
         color = "red"        
     #if row['Backtest'] == 'Venta':        
     #    color = "red"    
@@ -139,7 +144,7 @@ for index, row in data.iterrows():
 
 
 
-ax.set_title(f'Gráfico de SMA_{short_sma}, SMA_{long_sma} y Close para {ticker_symbol}')
+ax.set_title(f'Gráfico de SMA_{short_sma}, SMA_{long_sma} y Close para {TICKER_SYMBOL}')
 ax.set_xlabel('Fecha')
 ax.set_ylabel('Precio')
 ax.legend()
